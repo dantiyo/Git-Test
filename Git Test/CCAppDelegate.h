b4b1fc21a34f5e0489f9ce9e2cecc3eb1de07b23
Roger Acton <Acton@Mac-Pro-Mr-R-2.local>
2014-08-15 03:50:01 +0200

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Roger Acton on 15-08-14.
+//  Copyright (c) 2014 Roger Marlo Acton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Roger Acton on 15-08-14.
+//  Copyright (c) 2014 Roger Marlo Acton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
